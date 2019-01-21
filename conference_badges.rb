f76826b8e25708d070ddc11734d5672a50b7d0ff
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-21 17:56:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  newBadge =[]
+array.each do |name|
+  newBadge << "Hello, my name is #{name}."
+end
+return newBadge
+end
+ 
+ 
+ def assign_rooms(array)
+   newArr = []
+  array.each do |name|
+    newArr << "Hello, #{name}! You'll be assigned to room #{room}!"
+ end
+ return newArr
+ end
